9a8d1c68646bb43222b6a221f0c8aabe4f3755db
prabveer <81586936+prabveer@users.noreply.github.com>
2023-09-01 18:38:17 -0500

    started working on JS code for libary project

---
+++ script.js
@@ -0,0 +1,20 @@
+const myLibrary = [];
+
+class Book {
+    // constructor //
+    constructor(name, author, pages, status)
+    {
+        this.name = name;
+        this.author = author;
+        this.pages = pages;
+        this.status = status;
+    }
+    // getters //
+    get name () {
+        
+    }
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}

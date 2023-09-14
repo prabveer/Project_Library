4ca6a68f0d39bbca15175fa848dff925c2b4d343
prabveer <81586936+prabveer@users.noreply.github.com>
2023-09-14 17:34:45 -0500

    worked on javascript portion of library

--- script.js
+++ script.js
@@ -1,82 +1,72 @@
 // Selectors //
-const bookshelf = document.querySelector(".bookshelf .content")
-const Name	 = document.querySelector(".bookName")
-const Author = document.querySelector(".bookAuthor")
-const Pages = document.querySelector(".bookPages")
-const Status = document.querySelector(".bookStatus")
-const myLibrary = [
+const bookshelf = document.querySelector(".bookshelf .content");
+const Name	 = document.querySelector(".bookName");
+const Author = document.querySelector(".bookAuthor");
+const Pages = document.querySelector(".bookPages");
+const Status = document.querySelector(".bookStatus");
+
+const showButton = document.getElementById("showDialog");
+const favDialog = document.getElementById("favDialog");
+const confirmButton = document.getElementById("confirmBtn");
+
+const formName	 = document.querySelector("#formBookName");
+const formAuthor = document.querySelector("#formAuthorName");
+const formPages = document.querySelector("#formPageTotal");
+const formStatus = document.querySelector("#readstatus");
+let i = 0;
+let myLibrary = [
   {
     name: "Frankenstein",
     author: "Mary Shelley",
     pages: 240,
     status: true
+  },
+  {
+    name: "Why",
+    author: "do i Suffer",
+    pages: 420,
+    status: false
   }
 ];
 
-class Book {
-    // constructor //
-    constructor(name, author, pages, status)
-    {
-        this.name = name;
-        this.author = author;
-        this.pages = pages;
-        this.status = status;
-    }
-    // getters //
-    get name() {
-      return this.name
-    }
-    get author() {
-      return this.author
-    }
-    get pages() {
-      return this.pages
-    }
-    get status() {
-      return this.status
-    }
+function Book (name, author, pages, status) {
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
 }
-
+const clearForm = () => {
+  formName.value = "";
+  formAuthor.value = "";
+  formPages.value = "" ;
+}
 function addBookToLibrary() {
-  // do stuff here
+  let N = formName.value;
+  let A = formAuthor.value;
+  let P = formPages.value;
+  let R = formStatus.value;
+  let newBook = new Book(N, A, P, R)
+  myLibrary.push(newBook)
 }
-
-myLibrary.forEach(function (arrayItem)
+function updateTable()
 {
-  var tr = document.createElement('tr')
-  tr.innerHTML = `<td>${arrayItem.name}</td>
-  <td>${arrayItem.author}</td> 
-  <td>${arrayItem.pages}</td> 
-  <td>${arrayItem.status ? 'Read' : 'Not Read'}</td> `;
-  bookshelf.appendChild(tr)
-}) 
-
-const showButton = document.getElementById("showDialog");
-const favDialog = document.getElementById("favDialog");
-const outputBox = document.querySelector("output");
-const selectEl = favDialog.querySelector("select");
-const confirmBtn = favDialog.querySelector("#confirmBtn");
-
+  myLibrary.forEach(function (arrayItem)
+  {
+    var tr = document.createElement('tr')
+    tr.innerHTML = `<td>${arrayItem.name}</td>
+    <td>${arrayItem.author}</td> 
+    <td>${arrayItem.pages}</td> 
+    <td>${arrayItem.status ? 'Read' : 'Not Read'}</td> `;
+    bookshelf.appendChild(tr)
+  }) 
+}
+//updateTable();
 // "Show the dialog" button opens the <dialog> modally
 showButton.addEventListener("click", () => {
   favDialog.showModal();
 });
-
-// "Favorite animal" input sets the value of the submit button
-selectEl.addEventListener("change", (e) => {
-  confirmBtn.value = selectEl.value;
-});
-
-// "Cancel" button closes the dialog without submitting because of [formmethod="dialog"], triggering a close event.
-favDialog.addEventListener("close", (e) => {
-  outputBox.value =
-    favDialog.returnValue === "default"
-      ? "No return value."
-      : `ReturnValue: ${favDialog.returnValue}.`; // Have to check for "default" rather than empty string
-});
-
-// Prevent the "confirm" button from the default behavior of submitting the form, and close the dialog with the `close()` method, which triggers the "close" event.
-confirmBtn.addEventListener("click", (event) => {
-  event.preventDefault(); // We don't want to submit this fake form
-  favDialog.close(selectEl.value); // Have to send the select box value here.
+confirmButton.addEventListener("click", () => {
+  addBookToLibrary();
+  updateTable();
+  clearForm();
 });
